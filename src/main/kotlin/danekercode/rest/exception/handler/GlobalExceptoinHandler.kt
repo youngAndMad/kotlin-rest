be7487e47fe2ef08e21a07136d880c2ed5edb2e9
daneker <kkraken2005@gmail.com>
2023-08-19 16:05:24 +0600

    docker

--- src/main/kotlin/danekercode/rest/exception/handler/GlobalExceptoinHandler.kt
+++ src/main/kotlin/danekercode/rest/exception/handler/GlobalExceptoinHandler.kt
@@ -2,6 +2,8 @@ package danekercode.rest.exception.handler
 
 import danekercode.rest.exception.EmployeeNotFoundException
 import danekercode.rest.exception.InvalidEmployeeRequestException
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class GlobalExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException::class)
-    fun handle(e: EmployeeNotFoundException) : ResponseEntity<Any> = ResponseEntity.status(404).body(e.message)
+    fun handle(e: EmployeeNotFoundException) : ProblemDetail = ProblemDetail
+            .forStatusAndDetail(HttpStatusCode.valueOf(404), e.message.toString())
 
     @ExceptionHandler(InvalidEmployeeRequestException::class)
-    fun handle(e: InvalidEmployeeRequestException) : ResponseEntity<Any> = ResponseEntity.status(400).body(e.message)
+    fun handle(e: InvalidEmployeeRequestException) : ProblemDetail = ProblemDetail
+            .forStatusAndDetail(HttpStatusCode.valueOf(400), e.message.toString())
 }
