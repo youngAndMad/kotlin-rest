725d93043ac8641cd90cb707e732e4c4aa547259
youngandmad <kkraken2005@gmail.com>
2023-12-07 22:27:35 +0600

    cors config

---
+++ src/main/kotlin/danekercode/rest/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package danekercode.rest.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun addCorsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOriginPatterns("/**")
+                    .allowCredentials(true)
+            }
+        }
+    }
+
+}
